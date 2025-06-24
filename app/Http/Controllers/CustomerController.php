011232ba335b5e2da023e98068a8f7b43e438591
Viktor Čaloud <x260514@stud.fit.vut.cz>
2025-06-24 23:29:27 +0200

    Add Customer resource & collection, update controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
-//use App\Http\Requests\StoreCustomerRequest;
-//use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Resources\CustomerResource;
+use App\Http\Resources\CustomerCollection;
 
 class CustomerController extends Controller
 {
@@ -13,7 +15,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return Customer::with('groups')->get();
+        return new CustomerCollection(Customer::with('groups')->get());
     }
 
     /**
@@ -22,5 +24,32 @@ class CustomerController extends Controller
     public function show($id)
     {
         return Customer::with('groups')->findOrFail($id);
+    }
+
+    public function store(StoreCustomerRequest $request)
+    {
+        $customer = Customer::create($request->validated());
+        
+        $customer->groups()->sync($request->input('group_ids', []));
+
+        return new CustomerResource($customer);
+    }
+
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        $customer->update($request->validated());
+
+        if ($request->has('group_ids')) {
+            $customer->groups()->sync($request->input('group_ids', []));
+        }
+        
+        return new CustomerResource($customer);
+    }
+
+
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return response()->noContent();
     }
 }
