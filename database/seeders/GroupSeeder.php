bfe582ad5ab9a5a924975f83f318b2e7a599c6f0
Viktor Čaloud <x260514@stud.fit.vut.cz>
2025-06-24 22:31:29 +0200

    Create GroupSeeder and update DatabaseSeeder

---
+++ database/seeders/GroupSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Group;
+
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $groups = [
+            ['name' => 'Alpha'],
+            ['name' => 'Beta'],
+            ['name' => 'Gamma'],
+        ];
+
+        foreach ($groups as $group) {
+            Group::create($group);
+        }
+    }
+}
