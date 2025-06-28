eb9b6043e7f9d3e35f5dd0c040a2cb21320acfde
Viktor Čaloud <x260514@stud.fit.vut.cz>
2025-06-28 15:27:56 +0200

    Initial project cleanup

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\GroupController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// RESTful routes (index, store, show, update, destroy)
+/**
+ * RESTful API routes for Customer/Group resource.
+ * (index, store, show, update, destroy)
+ */
 Route::apiResource('customers', CustomerController::class);
 Route::apiResource('groups', GroupController::class);
